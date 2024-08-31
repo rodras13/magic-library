9b013cfc9d3ff5b1a57e236c0280cb336bcbc0b3
rodras13 <koscinskirodrigo@gmail.com>
2024-08-31 23:49:18 +0200

    Vite vanilla init

---
+++ frontend/main.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+import { createRoot } from 'react-dom/client';
+import { App } from './src/App';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <App />
+);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+export function App () {
+  return (
+    <h1>Bienvenido a React ⚛️</h1>
+  );
+}
